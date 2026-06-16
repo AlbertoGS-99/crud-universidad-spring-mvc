7db25be897766548ff6f3703c8b6d8c12f1ab166
AlbertoGS-99 <Albertogonzas1999@gmail.com>
2026-06-16 14:49:53 +0200

    Final de dia, Empezando con los Services.

--- src/main/java/com/example/CrudUniversidadSpringMvcApplication.java
+++ src/main/java/com/example/CrudUniversidadSpringMvcApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudUniversidadSpringMvcApplication {
 
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CrudUniversidadSpringMvcApplication.class, args);
 	}
 
+	
 }
---
+++ src/main/java/com/example/entities/Correo.java
@@ -0,0 +1,32 @@
+package com.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="correos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Correo {
+
+     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private int id;
+
+    private String direccion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Estudiante estudiantes;
+
+}
